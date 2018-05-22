750a4d4bc956f9038bb548a713072ede77929fa6
Luke Conley <luke.sovern.conley@gmail.com>
2018-05-22 17:42:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  new_dwarves = []
+  array.each_with_index do |dwarf, index|
+    dwarf = "#{index + 1}. #{dwarf}"
+    new_dwarves << dwarf
+  end
+  half = new_dwarves.length / 2
+  puts new_dwarves[0..half]
+end
+binding.pry
+def summon_captain_planet(calls)
+  calls.collect do |call|
+    call = "#{call.capitalize}!"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(list)
+  list.any? do |shout|
+    shout.length > 4
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def find_the_cheese(board)
+  cheese_types = ["cheddar", "gouda", "camembert", "swiss"]
+  board.find do |item|
+    cheese_types.include? item
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def words_with_b(array)
+  array.select do |word|
+    word.include? "b"
+  end
+
 end
